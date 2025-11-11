c6ab2653a3944cf9a942c6779a6d988853d09299
elattarynoha <elattarynohaila@gmail.com>
2025-11-11 20:20:24 +0100

    Product model-Reposotory

--- src/main/java/com/hafssa/gestion_commandes/config/SecurityConfiguration.java
+++ src/main/java/com/hafssa/gestion_commandes/config/SecurityConfiguration.java
@@ -3,21 +3,20 @@ package com.hafssa.gestion_commandes.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfiguration {
 
-    private final UserDetailsService userService;
+    private final UserDetailsService UserService;
 
     public SecurityConfiguration(UserDetailsService userService) {
-        this.userService = userService;
+        this.UserService = userService;
     }
 
     @Bean
